aeb4e8f7f2c7dcf870bcc479a8c791088eeaf377
Princess585 <princessdkhombisa1994@gmail.com>
2024-08-29 09:12:42 +0000

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,46 @@
 #!/usr/bin/python3
 
+"""Script that reads stdin line by line and comp"""
+
 import sys
-import signal
 
-def print_stats(total_size, status_counts):
-    """Script that writes stdin line by line ans comp"""
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts):
-        if status_counts[status_code] > 0:
-            print(f"{status_code}: {status_counts[status_code]}")
 
-def process_line(line, total_size, status_counts):
-    """The total file size"""
-    try:
-        parts = line.split()
-        if len(parts) < 7:
-            return total_size, status_counts
+def printsts(dic, size):
+    """Total file size"""
 
-        file_size = int(parts[-1])
-        total_size += file_size
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-        status_code = parts[-2]
-        if status_code.isdigit() and int(status_code) in status_counts:
-            status_counts[int(status_code)] += 1
 
-    except Exception as e:
-        pass
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
-    return total_size, status_counts
+count = 0
+size = 0
 
-def main():
-    total_size = 0
-    status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-    def handle_interrupt(signal, frame):
-        """The number of lines by status code"""
-        print_stats(total_size, status_counts)
-        sys.exit(0)
+        stlist = line.split()
+        count += 1
 
-    signal.signal(signal.SIGINT, handle_interrupt)
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
 
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            total_size, status_counts = process_line(line, total_size, status_counts)
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
 
-            if line_count % 10 == 0:
-                print_stats(total_size, status_counts)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        print_stats(total_size, status_counts)
-
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    printsts(sts, size)
+    raise
