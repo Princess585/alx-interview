66bd9ebd31cdf41b1fa8a4b9fdaca08e8862102b
Princess585 <princessdkhombisa1994@gmail.com>
2024-09-12 09:15:59 +0000

    queens

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""N queens"""
+import sys
+
+
+def solve_queens_problem(board_size, col, N):
+    """Solve the board, size, cole and N problems"""
+
+    if col == N:
+        solution = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col] = 1
+            solve_nqueens(board, col + 1, N, solutions)
+            board[i][col] = 0
+
+    def place_queens(self, i, k):
+        """Return 1"""
+         for j in range(1, k):
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+
+def main():
+    """The main queens"""
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        board_size = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if board_size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solutions = solve_queens_problem(board_size)
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    main()
